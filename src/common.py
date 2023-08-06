2f20f06ade876854314695e1c055b9f591ad65ac
parampalsinghgill <parampalsinghgill@gmail.com>
2023-08-06 00:08:46 -0700

    - set the color to auto reset

--- src/common.py
+++ src/common.py
@@ -1,18 +1,18 @@
 import sys
-from colorama import Fore
+from colorama import Fore, init
 
 
 def print_options_in_dict(dict_to_print):
     """Display the menu options and ask user choice"""
     print()
     for item in dict_to_print.items():
-        print(Fore.GREEN + "{} : {}".format(item[0], item[1]))
+        print("{} : {}".format(item[0], item[1]))
     print()
 
 
 def highlight_input_option(option):
     """Highlights the option user chose and what he is doing currently"""
-    print("\n######### Entered option to '{}' #########".format(option.lower()))
+    print(Fore.GREEN + "\n######### Entered option to '{}' #########".format(option.lower()))
 
 
 def quit_app():
